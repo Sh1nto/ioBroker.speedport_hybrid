f38e4a77b1f5c2346439c1cbe1de9c05d3b9c10e
Sh1nto <github@midl.de>
2019-11-11 11:40:32 +0000

    Config settings

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const utils = require('@iobroker/adapter-core');
 
 // Load your modules here, e.g.:
 // const fs = require("fs");
+const http = require('http');
 
 class SpeedportHybrid extends utils.Adapter {
 
